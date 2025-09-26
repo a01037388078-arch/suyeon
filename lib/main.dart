736033bcd2cdba2b21d2c2915473c70532410bc9
suyeon <a01037388078@gmail.com>
2025-09-26 10:40:44 +0900

    개인과제 1번

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  int score = 84;
+  String grade;
+  if (score >= 90 && score <= 100) {
+    grade = "A등급";
+  } else if (score >= 80 && score < 90) {
+    grade = "B등급";
+  } else {
+    grade = "C등급";
+  }
+
+   print("필수문제 1번");
+   print("이 학생의 점수는 $score점 이며, 등급은 $grade입니다.");
+}
